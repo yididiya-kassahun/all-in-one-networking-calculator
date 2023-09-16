ed506701315b8945f5118dd4a0bae6ebc520a34e
yididiya <57259174+yididiya-kassahun@users.noreply.github.com>
2023-09-16 18:29:47 +0300

    binary to decimal converter done

--- controllers/binaryController.js
+++ controllers/binaryController.js
@@ -1,6 +1,43 @@
 exports.binaryPage = (req, res, next) => {
     res.render("binaryCalc", {
       pageTitle: "binary calculator page",
+      decimalValue:0
     });
   };
   
+
+  exports.binaryToDecimal = (req,res,next) => {
+      const binaryVal = req.body.binary;
+      
+      let binaryArr = [];
+      binaryArr = Array.from(binaryVal);
+      let constNum = [];
+      let values = {};
+      let num = 1;
+      for (let i = binaryArr.length-1; i >= 0; i--) {
+         num *= 2;
+         constNum.push(num);
+
+        //  console.log(num);
+        //  console.log(binaryArr[i]);
+      }
+      binaryArr.reverse();
+
+      for (let j = 0; j < binaryArr.length; j++) {
+         values[constNum[j]] = binaryArr[j];
+      }
+
+      let sum = 0;
+      for(const key in values){
+     //   console.log(key,values[key]);
+        if(values[key] == 1){
+          sum +=parseInt(key);
+        }
+      }
+     // console.log('the sum is '+sum);
+     return res.render("binaryCalc",{
+      pageTitle: "binary calculator page",
+      decimalValue: sum
+     })
+
+  };
--- routes/binaryRouter.js
+++ routes/binaryRouter.js
@@ -4,5 +4,7 @@ const binaryController = require("../controllers/binaryController");
 const router = express.Router();
 
 router.get('/binary', binaryController.binaryPage);
+router.post('/convertBtoD', binaryController.binaryToDecimal);
+
 
 module.exports = router;
