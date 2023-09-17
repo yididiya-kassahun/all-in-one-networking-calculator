9a03c5f3841138df0bd94d9edfbec71a5525fef8
yididiya <57259174+yididiya-kassahun@users.noreply.github.com>
2023-09-17 10:12:10 +0300

    decimal to binary done

--- controllers/binaryController.js
+++ controllers/binaryController.js
@@ -1,7 +1,8 @@
 exports.binaryPage = (req, res, next) => {
     res.render("binaryCalc", {
       pageTitle: "binary calculator page",
-      decimalValue:0
+      decimalValue:'',
+      binaryValue:''
     });
   };
   
@@ -14,12 +15,13 @@ exports.binaryPage = (req, res, next) => {
       let constNum = [];
       let values = {};
       let num = 1;
+      console.log(binaryArr);
       for (let i = binaryArr.length-1; i >= 0; i--) {
-         num *= 2;
-         constNum.push(num);
+        constNum.push(num);
+        num *= 2;
 
-        //  console.log(num);
-        //  console.log(binaryArr[i]);
+         console.log(num);
+         console.log(binaryArr[i]);
       }
       binaryArr.reverse();
 
@@ -37,7 +39,35 @@ exports.binaryPage = (req, res, next) => {
      // console.log('the sum is '+sum);
      return res.render("binaryCalc",{
       pageTitle: "binary calculator page",
-      decimalValue: sum
+      decimalValue: sum,
+      binaryValue:''
      })
+
+  };
+
+  exports.decimalToBinary = (req,res,next)=> {
+   let decimalVal = parseInt(req.body.decimalVal);
+   let reminder = [];
+   let check = [];
+  
+   let result = 0;
+   let nextNum = 0;
+
+   while (decimalVal >= 1) {
+    nextNum = decimalVal;
+    result = decimalVal%2;
+    reminder.push(result);
+
+    check.push(nextNum);
+    nextNum = Math.floor(decimalVal/2);
+    decimalVal = nextNum;
+   }
+   binaryValue = reminder.reverse();
+ 
+  return res.render("binaryCalc",{
+    pageTitle: "binary calculator page",
+    decimalValue:'',
+    binaryValue: binaryValue
+   })
 
   };
--- routes/binaryRouter.js
+++ routes/binaryRouter.js
@@ -5,6 +5,6 @@ const router = express.Router();
 
 router.get('/binary', binaryController.binaryPage);
 router.post('/convertBtoD', binaryController.binaryToDecimal);
-
+router.post('/convertDtoB', binaryController.decimalToBinary);
 
 module.exports = router;
