f00aa15db5a86121edf4df46caf7014e8a9f0e03
yididiya <57259174+yididiya-kassahun@users.noreply.github.com>
2023-09-21 19:39:45 +0300

    math code and UI added

--- routes/mathRouter.js
+++ routes/mathRouter.js
@@ -4,5 +4,6 @@ const mathContoller = require("../controllers/mathController");
 const router = express.Router();
 
 router.get('/mathPage', mathContoller.mathPage);
+router.post('/ftod', mathContoller.mathPage);
 
 module.exports = router;
